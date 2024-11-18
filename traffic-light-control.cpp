aa9dd7c6b9681b2a713e729f6352003de10a7cca
Israel Alagbe <israelalagbe53@gmail.com>
2024-11-18 18:47:52 +0100

    Added code for traffic light control

---
+++ traffic-light-control.cpp
@@ -0,0 +1,28 @@
+const int redLed = 6;
+const int yellowLed = 5;
+const int greenLed = 4;
+const unsigned long int timeout = 3000; //  3 seconds(s)
+
+void turnOnLed(int pin) {
+  digitalWrite(redLed, LOW);
+  digitalWrite(yellowLed, LOW);
+  digitalWrite(greenLed, LOW);
+  digitalWrite(pin, HIGH);
+}
+
+void setup()
+{
+  pinMode(redLed, OUTPUT);
+  pinMode(yellowLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+}
+
+void loop()
+{
+  turnOnLed(redLed);
+  delay(3000); 
+  turnOnLed(yellowLed);
+  delay(3000);
+  turnOnLed(greenLed);
+  delay(3000);
+}
